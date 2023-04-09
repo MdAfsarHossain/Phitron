c3bec4c42a69a5e22b3e2421c308955568b97a5b
AfsarHossain <afsar291722@gmail.com>
2023-04-09 15:52:04 +0600

    Week 1 Lab 1 Question 9 answer added

---
+++ Semester 1/Course 2 [Programming in C]/Week 1 [Introduction in C]/Lab 1/Question_9.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    if(x>0 && y>0)
+    {
+        printf("The coordinate point (%d,%d) lies in the First quadrant.\n", x, y);
+    }
+    else if(x<0 && y>0)
+    {
+        printf("The coordinate point (%d,%d) lies in the Second quadrant.\n", x, y);
+    }
+    else if(x<0 && y<0)
+    {
+        printf("The coordinate point (%d,%d) lies in the Third quadrant.\n", x, y);
+    }
+    else
+    {
+        printf("The coordinate point (%d,%d) lies in the Fourth quadrant.\n", x, y);
+    }
+    return 0;
+}
