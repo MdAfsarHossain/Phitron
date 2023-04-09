6ee632c9ae6f3e1fccaca2febd39a6f4513bcd75
AfsarHossain <afsar291722@gmail.com>
2023-04-09 15:49:55 +0600

    Week 1 Lab 1 Question 1 answer added

---
+++ Semester 1/Course 2 [Programming in C]/Week 1 [Introduction in C]/Lab 1/Question_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a==b)
+    {
+        printf("Number1 and Number2 are equal\n");
+    }
+    else
+    {
+        printf("Number1 and Number2 are not equal.\n");
+    }
+
+    return 0;
+}
