b5ff98031c479ba2a55ee9533c0f8c690fe3d3ba
AfsarHossain <afsar291722@gmail.com>
2023-04-11 23:01:25 +0600

    Week 1 Practice 1 question 1 added

---
+++ Semester 1/Course 2 [Programming in C]/Week 1 [Introduction in C]/Practice 1/Problem_1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hi there /\\.\tI hope you are doing great and practicing well.\nNow this is a simple task you need to do.\tJust care for the indentation.\nShow 100%% of sincerity.\n");
+
+    return 0;
+}
