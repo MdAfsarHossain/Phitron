4b9fc9b6f9679a2cb52f9490c49165c4e66089da
AfsarHossain <afsar291722@gmail.com>
2023-04-11 23:03:12 +0600

    Week 1 Practice 1 question 5 added

---
+++ Semester 1/Course 2 [Programming in C]/Week 1 [Introduction in C]/Practice 1/Problem_5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    scanf("%c", &ch);
+    //getchar();
+    //while(scanf("%c", &ch))
+    //{
+    printf("%d\n", ch);
+
+    //  getchar();
+    //}
+
+    return 0;
+}
