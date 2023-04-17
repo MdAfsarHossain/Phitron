d03f83821d5012b9abc315349341493540ada68f
AfsarHossain <afsar291722@gmail.com>
2023-04-17 13:32:36 +0600

    Week 3 Practice 2 Problem 2 added

---
+++ Semester 1/Course 2 [Programming in C]/Week 3 [Array and Nested Loop]/Practice 2/Problem_2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int input[n+1];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &input[i]);
+    }
+
+    int max = input[0];
+
+    for(int i=1; i<n; i++)
+    {
+        if(input[i]>max)
+        {
+            max = input[i];
+        }
+    }
+
+    int ara[max+1];
+
+    for(int i=0; i<=max; i++)
+    {
+        ara[i] = 0;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        ara[input[i]] += 1;
+    }
+
+    for(int i=0; i<=max; i++)
+    {
+        if(ara[i] == 1)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
