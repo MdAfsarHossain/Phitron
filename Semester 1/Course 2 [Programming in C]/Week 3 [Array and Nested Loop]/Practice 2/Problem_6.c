f54980f2e14994854e37c7af1ba19a6ecd50cfa1
AfsarHossain <afsar291722@gmail.com>
2023-04-17 13:33:45 +0600

    Week 3 Practice 2 Problem 6 added

---
+++ Semester 1/Course 2 [Programming in C]/Week 3 [Array and Nested Loop]/Practice 2/Problem_6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=1; j<=n-i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+        for(int k=0; k<i+1; k++)
+        {
+            printf(" ");
+        }
+    }
+}
