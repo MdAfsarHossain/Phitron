743a11c06d3acce44609cd8f66b594cd55c35ef8
AfsarHossain <afsar291722@gmail.com>
2023-04-20 12:43:24 +0600

    Week 4 Practice 1 Number of Equal Added

---
+++ Semester 1/Course 2 [Programming in C]/Week 4 [String and 2D Array]/Practice 1/Number_of_Equal.cpp
@@ -0,0 +1,37 @@
+/**
+https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/C
+**/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll n, m;
+    cin>>n>>m;
+
+    ll ara1[n+1], ara2[m+1];
+
+    for(ll i=0; i<n; i++)
+    {
+        cin>>ara1[i];
+    }
+
+    for(ll i=0; i<m; i++)
+    {
+        cin>>ara2[i];
+    }
+
+    ll cnt = 0;
+    for(ll i=0; i<m; i++)
+    {
+        for(ll j=0; j<n; j++)
+        {
+            if(ara2[i] == ara1[j])
+                cnt++;
+        }
+    }
+
+    cout<<cnt<<endl;
+
+    return 0;
+}
