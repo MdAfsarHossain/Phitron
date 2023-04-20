c37c87997136d15ef8c366c1c4329b0090c30ef6
AfsarHossain <afsar291722@gmail.com>
2023-04-20 12:46:33 +0600

    Week 4 Practice 1 Problem 4 Added

---
+++ Semester 1/Course 2 [Programming in C]/Week 4 [String and 2D Array]/Practice 1/Problem_4.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+
+    int ara1[n+1][m+1];
+    int ara2[n+1][m+1];
+    int ara3[n+1][m+1];
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            scanf("%d", &ara1[i][j]);
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            scanf("%d", &ara2[i][j]);
+        }
+    }
+
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            ara3[i][j] = ara1[i][j] + ara2[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            printf("%d ", ara3[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
