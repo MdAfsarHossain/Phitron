f8f0fed92cd0bc5b09c36b8d8a971c67d4a9aa69
AfsarHossain <afsar291722@gmail.com>
2023-04-25 11:18:37 +0600

    Week 8 Module 28 Realloc Added

---
+++ Semester 1/Course 2 [Programming in C]/Week 8 [Pointer and Structure]/Module 28 [More About Pointer]/Realloc.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int* ptr;
+    ptr = (int*) malloc(n*sizeof(int));
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", (ptr+i));
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", *(ptr+i));
+    }
+
+    ptr = realloc(ptr, (n+5)*sizeof(int));
+
+
+
+    free(ptr);
+
+    return 0;
+}
