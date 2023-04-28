43d345a755fc17320a7d075992c807326df5104f
AfsarHossain <afsar291722@gmail.com>
2023-04-28 14:58:31 +0600

    Assignment 1 Problem 4 added

---
+++ Semester 1/Course 2 [Programming in C]/Assignment 1/Problem_4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    long long int n, sum=0, r;
+
+    scanf("%lld", &n);
+
+    while(n!=0)
+    {
+        r = n % 10;
+        sum += r;
+        n = n / 10;
+    }
+
+    printf("%lld", sum);
+
+    return 0;
+}
+
