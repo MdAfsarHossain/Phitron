3e31b99de0657a0f7b5d17b4a1a338bf14ff2db7
AfsarHossain <afsar291722@gmail.com>
2023-04-28 15:00:13 +0600

    Assignment 1 Debug added

---
+++ Semester 1/Course 2 [Programming in C]/Assignment 1/debug.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    //scanf("%d", &n);
+
+    int ara[] = {10, 20, 30, 40, 50};
+    //for(int i=0; i<n; i++)
+    {
+      //  scanf("%d", &ara[i]);
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", ara[i]);
+    }
+
+    return 0;
+}
