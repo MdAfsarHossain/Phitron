06bd6d4f3ef81c9da125fd40c9f0372df3cc271b
AfsarHossain <afsar291722@gmail.com>
2023-04-30 08:18:47 +0600

    Assignment 3 Test Added

---
+++ Semester 1/Course 2 [Programming in C]/Assignment 3/Test.c
@@ -0,0 +1,85 @@
+//#include<stdio.h>
+//struct Course {
+//    char name[];
+//    int price;
+//};
+//
+//int main()
+//{
+//    struct Course c1 ={"C Programming",5000};
+//    return 0;
+//}
+
+//#include<stdio.h>
+//
+//struct Point
+//{
+//  int x, y, z;
+//};
+//
+//int main()
+//{
+//  struct Point p = {.y = 5, .z = 10, .x = 20};
+//  printf("%d", p.y);
+//  return 0;
+//}
+
+//# include <stdio.h>
+//# include <string.h>
+//
+//struct Name
+//{
+//  char str[20];
+//};
+//
+//int main()
+//{
+//  struct Name st1, st2;
+//  strcpy(st1.str,"ProgrammingHero");
+//  st2 = st1;
+//  st1.str[0] = 'p';
+//
+//  printf("%s",st2.str);
+//
+//  return 0;
+//}
+
+//#include<stdio.h>
+//struct st
+//{
+//    char x;
+//    int ar[6];
+//    int a;
+//    double b;
+//};
+//
+//int main()
+//{
+//    printf("%d", sizeof(struct st));
+//    return 0;
+//}
+
+//#include<stdio.h>
+//
+//struct st
+//{
+//    int x;
+//    int ar[5];
+//};
+//
+//int main()
+//{
+//    printf("%d", sizeof(struct st));
+//    return 0;
+//}
+
+//1 -> 1
+//2 -> 1
+//3 -> 1
+//4 -> 1
+//5 -> 1
+//6 -> 1
+//7 -> 1
+//8 -> 1
+//9 -> 1
+//10 -> 1
