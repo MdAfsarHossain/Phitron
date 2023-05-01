c9e41b01e75e1d78e43b2ebf4361f24578f4f6e8
AfsarHossain <afsar291722@gmail.com>
2023-05-01 10:48:21 +0600

    Programming in C Common Code added

---
+++ Semester 1/Course 2 [Programming in C]/Common Code/Duplicate_Array4.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int input[n+1];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &input[i]);
+    }
+
+    int ara[n+1];
+    int uni_index = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        ara[i] = 0;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        int select = input[i];
+        int found = 0;
+
+        for(int j=0; j<n; j++)
+        {
+            if(ara[j] == select)
+            {
+                found = 1;
+            }
+        }
+
+        if(found == 0)
+        {
+            ara[uni_index] = input[i];
+            uni_index++;
+        }
+    }
+
+    for(int i=0; i<uni_index; i++)
+    {
+        printf("%d ", ara[i]);
+    }
+
+    return 0;
+}
+
