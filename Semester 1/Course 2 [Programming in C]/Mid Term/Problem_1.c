17fde12a832534e7fcde99eea33a240d717992f3
AfsarHossain <afsar291722@gmail.com>
2023-05-01 10:50:09 +0600

    Mid Term Xm Question 1 added

---
+++ Semester 1/Course 2 [Programming in C]/Mid Term/Problem_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n = 10;
+    int x = 5;
+    int index = 0;
+    int a[10] = {12, 7, 3, 71, 2, 43, 38, 23, 45, 22};
+    int b[10];
+    for (int i=0; i<n; i++)
+    {
+        //Write Code Here
+        b[i] = a[i] + x;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", b[i]);
+    }
+
+    return 0;
+}
+
