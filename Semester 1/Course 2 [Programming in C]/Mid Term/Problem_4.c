a01db56850e3eff61c625343592e3b9ee99efd7f
AfsarHossain <afsar291722@gmail.com>
2023-05-01 10:50:57 +0600

    Mid Term Xm Question 4 answer added

---
+++ Semester 1/Course 2 [Programming in C]/Mid Term/Problem_4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    a[0] = 98;
+    a[1] = 97;
+    a[2] = 'n';
+    a[3] = 'a';
+    a[4] = 'n';
+    a[5] = 'a';
+    a[6] = '\0';
+
+    puts(a);
+
+    //banana
+    return 0;
+}
+
+
+
