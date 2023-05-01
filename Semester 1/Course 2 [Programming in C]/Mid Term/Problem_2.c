fc828eae3250a3eef2af1d51523564655f9e316d
AfsarHossain <afsar291722@gmail.com>
2023-05-01 10:50:33 +0600

    Mid Term Xm Question 2 answer added

---
+++ Semester 1/Course 2 [Programming in C]/Mid Term/Problem_2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    char s[] = "america";
+    puts(s);
+
+    /***
+    "america" is a character array.
+    Character array declare this format
+    char character_array_name[character_array_size];
+    But here write only char s.
+    char s means it takes input only one character.
+    But here give a string character.
+    Here a character_array_size is missing,
+    And In C programming we close every line with ;.
+    But here ; also missing.
+
+    So, We need to declare this code like.
+    Correct Code:
+    char s[] = "america";
+    ***/
+    return 0;
+}
+
