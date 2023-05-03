f57d2489287928619792fcbc76772fda6c0c98cb
AfsarHossain <afsar291722@gmail.com>
2023-05-03 23:25:15 +0600

    Contest 1 Problem 5 Solution added

---
+++ Semester 1/Course 2 [Programming in C]/Contest/Contest 1/P5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    if(a>b)
+    {
+
+    printf("%d\n",a-b);
+    }
+    else
+    {
+
+    printf("%d\n",b-a);
+    }
+
+
+    return 0;
+}
+
