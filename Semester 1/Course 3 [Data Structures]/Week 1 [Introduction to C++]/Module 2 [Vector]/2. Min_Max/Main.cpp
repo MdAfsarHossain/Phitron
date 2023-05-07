34bb7ebc71afedcff4631292620670d09d5a1404
AfsarHossain <afsar291722@gmail.com>
2023-05-07 19:16:58 +0600

    Vector min max file added

---
+++ Semester 1/Course 3 [Data Structures]/Week 1 [Introduction to C++]/Module 2 [Vector]/2. Min_Max/Main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    a = 10;
+    b = 4;
+    c = 6;
+    d = 15;
+
+    int mini = min({a, b, c, d});
+
+    cout<<mini;
+
+    return 0;
+}
