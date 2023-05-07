200cfffdfa100cd022ff714eb0ef59b186bc18b9
AfsarHossain <afsar291722@gmail.com>
2023-05-07 19:11:57 +0600

    Basic IO reopen main file added

---
+++ Semester 1/Course 3 [Data Structures]/Week 1 [Introduction to C++]/Module 1 [C++ Basic]/1-2 Basic IO/1 [reopen]/Main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    string s;
+//    cin>>s;
+    getline(cin, s);
+
+    cout<<s<<endl;
+
+    return 0;
+}
+
+//ctrl + shift + c
