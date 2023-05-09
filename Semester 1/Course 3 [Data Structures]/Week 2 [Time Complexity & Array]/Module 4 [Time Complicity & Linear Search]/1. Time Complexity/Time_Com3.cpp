6848c2c9aab250701333ffb379ef25cacac8d6f5
AfsarHossain <afsar291722@gmail.com>
2023-05-09 19:20:45 +0600

    Time Complexity 3 added

---
+++ Semester 1/Course 3 [Data Structures]/Week 2 [Time Complexity & Array]/Module 4 [Time Complicity & Linear Search]/1. Time Complexity/Time_Com3.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+Time Complexity: O(n^2)
+Memory Complexity: O(n)
+**/
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+
+    for(int i=0; i<n; i++)
+    {
+        string ans = "NO";
+        for(int j=0; j<n; j++)
+        {
+            if(i==j)
+                continue;
+            if(a[i] == a[j])
+                ans = "YES";
+        }
+        cout<<"i = "<<i<<" "<<ans<<"\n";
+    }
+}
