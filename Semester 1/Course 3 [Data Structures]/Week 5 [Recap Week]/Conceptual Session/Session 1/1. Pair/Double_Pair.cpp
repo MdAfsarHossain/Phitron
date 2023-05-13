d4adb75c8f948ab0257b79b3d1c933c0aa828f14
AfsarHossain <afsar291722@gmail.com>
2023-05-13 08:02:34 +0600

    Double Pair added

---
+++ Semester 1/Course 3 [Data Structures]/Week 5 [Recap Week]/Conceptual Session/Session 1/1. Pair/Double_Pair.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<pair<string, string>, int> p;
+
+    p.first.first = "Afsar";
+    p.first.second = "Hossain";
+    p.second = 25;
+
+    cout<<p.first.first<<" "<<p.first.second<<"\n";
+    cout<<p.second<<"\n";
+
+    return 0;
+}
