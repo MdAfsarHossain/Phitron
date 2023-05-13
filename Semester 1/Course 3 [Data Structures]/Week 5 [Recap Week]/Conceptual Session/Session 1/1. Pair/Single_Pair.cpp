ebb57e648fb2779cc3b1f189000833675d8e2ac0
AfsarHossain <afsar291722@gmail.com>
2023-05-13 08:02:48 +0600

    Single Pair added

---
+++ Semester 1/Course 3 [Data Structures]/Week 5 [Recap Week]/Conceptual Session/Session 1/1. Pair/Single_Pair.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<string, int> p;
+
+    p.first = "Afsar";
+    p.second = 25;
+
+    cout<<p.first<<"\n";
+    cout<<p.second<<"\n";
+
+    return 0;
+}
