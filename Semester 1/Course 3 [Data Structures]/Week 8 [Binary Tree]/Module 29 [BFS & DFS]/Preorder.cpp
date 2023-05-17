14c0af559c74dedcf55906c6acd49877491d0e68
AfsarHossain <afsar291722@gmail.com>
2023-05-17 19:03:40 +0600

    Preorder added

---
+++ Semester 1/Course 3 [Data Structures]/Week 8 [Binary Tree]/Module 29 [BFS & DFS]/Preorder.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int id;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+
+class BinaryTree
+{
+public:
+    Node* root;
+    Node* allNode[9];
+
+    BinaryTree()
+    {
+        root = NULL;
+    }
+
+    Node* CreateNewNode(int id)
+    {
+        Node* newNode = new Node;
+        newNode->id = id;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->parent = NULL;
+        return newNode;
+
+    }
+
+    void build_binary_tree()
+    {
+        for(int i=0; i<9; i++)
+        {
+            allNode[i] = CreateNewNode(i);
+        }
+
+        ///Tree: 1
+//        allNode[0]->left = allNode[1];
+//        allNode[0]->right = allNode[2];
+//
+//        allNode[1]->left = allNode[5];
+//        allNode[1]->parent = allNode[0];
+//
+//        allNode[5]->parent = allNode[1];
+//
+//        allNode[2]->left = allNode[3];
+//        allNode[2]->right = allNode[4];
+//        allNode[2]->parent = allNode[0];
+//
+//        allNode[3]->parent = allNode[2];
+//        allNode[4]->parent = allNode[2];
+//
+//        root = allNode[0];
+
+        ///Tree: 2
+        allNode[0]->left = allNode[1];
+        allNode[0]->right = allNode[2];
+
+        allNode[1]->left = allNode[3];
+        allNode[1]->right = allNode[4];
+        allNode[1]->parent = allNode[0];
+
+        allNode[2]->left = allNode[5];
+        allNode[2]->right = allNode[6];
+        allNode[2]->parent = allNode[0];
+        allNode[5]->parent = allNode[2];
+        allNode[6]->parent = allNode[2];
+
+        allNode[3]->left = allNode[7];
+        allNode[3]->right = allNode[8];
+        allNode[3]->parent = allNode[1];
+        allNode[4]->parent = allNode[1];
+
+        allNode[8]->parent = allNode[3];
+        allNode[9]->parent = allNode[3];
+
+        root = allNode[0];
+    }
+
+    ///Preorder: root + f(left) + f(right)
+    void Preorder(Node* a)
+    {
+        if(a == NULL)
+            return;
+        cout<<a->id<<" ";
+        Preorder(a->left);
+        Preorder(a->right);
+    }
+};
+
+int main()
+{
+    BinaryTree bt;
+
+    bt.build_binary_tree();
+    bt.Preorder(bt.root);
+    return 0;
+}
