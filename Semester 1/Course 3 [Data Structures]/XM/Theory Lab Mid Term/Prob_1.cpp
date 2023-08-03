367a1d77e0560e9dd0f8587d9414a5e1225f33f7
MdAfsarHossain <afsar291722@gmail.com>
2023-08-04 00:07:48 +0600

    Theory Lab Mid Term Problem 1 code added

---
+++ Semester 1/Course 3 [Data Structures]/XM/Theory Lab Mid Term/Prob_1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+
+    for(int i=n-1; i>=0; i--)
+        cout<<a[i]<<" ";
+
+    return 0;
+}
+/**
+5
+6 2 3 3 5
+
+**/
