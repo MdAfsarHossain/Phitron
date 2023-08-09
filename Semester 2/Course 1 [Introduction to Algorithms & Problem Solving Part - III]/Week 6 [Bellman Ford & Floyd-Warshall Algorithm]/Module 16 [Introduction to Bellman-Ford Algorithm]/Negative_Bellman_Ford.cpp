5834fcf0bbf7e55bccb641cf7aca6c115264742c
MdAfsarHossain <afsar291722@gmail.com>
2023-08-09 17:43:23 +0600

    Negative Bellman Code added

---
+++ Semester 2/Course 1 [Introduction to Algorithms & Problem Solving Part - III]/Week 6 [Bellman Ford & Floyd-Warshall Algorithm]/Module 16 [Introduction to Bellman-Ford Algorithm]/Negative_Bellman_Ford.cpp
@@ -0,0 +1,85 @@
+///Directed Graph
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+const int INF = 1e9;
+
+vector<pair<int, int>> adj_list[N];
+int d[N];
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+
+    for(int i=1; i<=n; i++)
+        d[i] = INF;
+
+    for(int i=0; i<m; i++)
+    {
+        int u, v, w;
+        cin>>u>>v>>w;
+
+        adj_list[u].push_back({v, w});
+    }
+
+    int src = 1;
+    d[src] = 0;
+
+    bool negative_cycle = false;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int node=1; node<=n; node++)
+        {
+            for(pair<int, int> adj_node: adj_list[node])
+            {
+                int u = node;
+                int v = adj_node.first;
+                int w = adj_node.second;
+
+                if(d[u] + w < d[v])
+                {
+                    d[v] = d[u] + w;
+                    if(i == n)
+                    {
+                        negative_cycle = true;
+                    }
+                }
+            }
+        }
+    }
+
+    if(negative_cycle == true)
+    {
+        cout<<"Graph has negative Cycle\n";
+    }
+    else
+    {
+        for(int i=1; i<=n; i++)
+        {
+            cout<<d[i]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
+
+/**
+3 3
+1 2 5
+2 3 -10
+3 1 1
+Graph has negative Cycle
+-----------------------------
+4 3
+1 2 5
+2 3 -1
+3 4 -2
+
+0 5 4 2
+
+
+**/
