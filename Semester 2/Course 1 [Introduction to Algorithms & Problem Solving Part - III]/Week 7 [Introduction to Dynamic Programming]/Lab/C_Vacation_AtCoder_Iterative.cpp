5a5eab6f56297fe59c783a7ce0b2eca098d76b71
MdAfsarHossain <afsar291722@gmail.com>
2023-08-09 17:51:55 +0600

    C Vacation AtCoder Iterative code added

---
+++ Semester 2/Course 1 [Introduction to Algorithms & Problem Solving Part - III]/Week 7 [Introduction to Dynamic Programming]/Lab/C_Vacation_AtCoder_Iterative.cpp
@@ -0,0 +1,61 @@
+/// https://atcoder.jp/contests/dp/tasks/dp_c
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+int happiness[N][4];
+int dp[N][4];
+
+/**
+- state: fun(n, x) -> maximum happiness till day-n
+                      if we pick task-x on day-n
+
+- recurrance: fun(n, x) = max {
+                            fun(n - 1, y) where x != y
+                            + happiness[n][x]
+                         }
+- base case: fun(1, x) = happiness[1][x]
+**/
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=3; j++)
+        {
+            cin>>happiness[i][j];
+        }
+    }
+
+    // 1. Handle base case
+    for(int task=1; task<=3; task++)
+        dp[1][task] = happiness[1][task];
+
+    // 2. Loop over the states
+    for(int day=1; day<=n; day++)
+    {
+        for(int current_task=1; current_task<=3; current_task++)
+        {
+            // 2.1. Calculate answer from smaller sub-problems
+            int max_profit = 0;
+
+            for(int last_task=1; last_task<=3; last_task++)
+            {
+                if(last_task != current_task)
+                {
+                    int current_profit = dp[day-1][last_task] + happiness[day][current_task];
+                    max_profit = max(max_profit, current_profit);
+                }
+            }
+            dp[day][current_task] = max_profit;
+        }
+    }
+
+    int ans = max({dp[n][1], dp[n][2], dp[n][3]});
+    cout<<ans<<"\n";
+
+    return 0;
+}
