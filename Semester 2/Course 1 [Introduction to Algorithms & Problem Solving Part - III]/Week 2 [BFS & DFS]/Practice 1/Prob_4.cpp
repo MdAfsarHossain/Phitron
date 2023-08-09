0a0d591f09a3ce5504a6ffc8dfe9abef72f57025
MdAfsarHossain <afsar291722@gmail.com>
2023-08-09 16:46:01 +0600

    Problem 4 code added

---
+++ Semester 2/Course 1 [Introduction to Algorithms & Problem Solving Part - III]/Week 2 [BFS & DFS]/Practice 1/Prob_4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(string s)
+{
+    int i=0;
+    cout<<reverse(s[i+1]);
+}
+
+int main()
+{
+    string s;
+    cin>>s;
+
+    reverse(s);
+    return 0;
+}
