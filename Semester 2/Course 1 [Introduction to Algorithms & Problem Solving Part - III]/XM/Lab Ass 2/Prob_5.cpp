6ce087c365ca4bb0470d19a64386ffe3e930351a
MdAfsarHossain <afsar291722@gmail.com>
2023-08-14 16:49:06 +0600

    Problem 5 code added

---
+++ Semester 2/Course 1 [Introduction to Algorithms & Problem Solving Part - III]/XM/Lab Ass 2/Prob_5.cpp
@@ -0,0 +1,46 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int ans = 0;
+int main()
+{
+    int n;
+    cin>>n;
+
+    while(n!=1)
+    {
+        if(n%2 == 1)
+        {
+            n = n -1;
+            ans++;
+        }
+        if(n%3==0)
+        {
+            n = n/3;
+            ans++;
+        }
+        if(n%2==0)
+        {
+            n = n / 2;
+            ans++;
+        }
+    }
+
+    cout<<ans<<"\n";
+
+    return 0;
+}
+*/
+/**
+11
+10
+5
+4
+2
+2
+
+11
+10
+9
+3
+3
+**/
