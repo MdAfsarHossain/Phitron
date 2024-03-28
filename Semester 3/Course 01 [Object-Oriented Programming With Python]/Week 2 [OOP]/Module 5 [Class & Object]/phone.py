2c7e01105bf3440b3b3f6304b25ef6db8171f588
MdAfsarHossain <afsar291722@gmail.com>
2024-03-28 23:01:17 +0600

    Phone file added

---
+++ Semester 3/Course 01 [Object-Oriented Programming With Python]/Week 2 [OOP]/Module 5 [Class & Object]/phone.py
@@ -0,0 +1,11 @@
+class Phone:
+    price = 19000
+    color = 'Blue'
+    brand = 'Oppe'
+
+my_phone = Phone()
+
+print(my_phone)
+print(my_phone.brand)
+print(my_phone.price)
+print(my_phone.color)
