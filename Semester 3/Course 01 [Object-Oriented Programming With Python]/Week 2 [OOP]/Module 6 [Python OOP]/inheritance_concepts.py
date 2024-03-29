61729c3715f1a151bd6f9f21495bb6515c9d18aa
MdAfsarHossain <afsar291722@gmail.com>
2024-03-29 23:00:15 +0600

    Inheritance concepts code added

---
+++ Semester 3/Course 01 [Object-Oriented Programming With Python]/Week 2 [OOP]/Module 6 [Python OOP]/inheritance_concepts.py
@@ -0,0 +1,18 @@
+# Base Class, Parent class
+class BaseClass:
+    pass
+
+# Derived class or child class:
+class DerivedClass(BaseClass):
+    pass
+
+'''
+1. Simple inheritance: parent class --> child class (Gadget ---> Phone) (Gadget --> Laptop)
+
+2. Multi-Level inheritance: Grand --> Parent --> Child (Vehicle ->> Bus --> ACBus) (Vehicle --> Truck --> PickupTruck)
+
+3. Multiple inheritance: Student (Family, School, Sports)
+
+4. Hybrid: Granda --> Father, Uncle, Aunty --> Child (Father, Uncle)
+
+'''
