2c41c3926e191a1579aa020f424cb6cf066db21a
MdAfsarHossain <afsar291722@gmail.com>
2024-03-31 23:28:02 +0600

    Singleton code added

---
+++ Semester 3/Course 01 [Object-Oriented Programming With Python]/Week 2 [OOP]/Module 7 [OOP More]/singleton.py
@@ -0,0 +1,26 @@
+# singleton --> One Single Instance
+# if you want a new instance, you will get the old one (already created) instance
+
+class Singleton:
+    __instance = None
+    def __init__(self):
+        if Singleton.__instance is None:
+            Singleton.__instance = self
+        else:
+            raise Exception('This is singleton. Already have an instance, use that one by calling get_instance method')
+
+    @staticmethod
+    def get_instance():
+        if Singleton.__instance is None:
+            Singleton()
+        return Singleton.__instance
+
+first = Singleton.get_instance()
+second = Singleton.get_instance()
+third = Singleton.get_instance()
+print(first)
+print(second)
+print(third)
+
+# last = Singleton()
+
