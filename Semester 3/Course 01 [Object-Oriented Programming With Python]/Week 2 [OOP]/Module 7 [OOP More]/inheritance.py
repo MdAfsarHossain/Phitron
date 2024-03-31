b21607734438321abb734008ba83b71d3d4a310c
MdAfsarHossain <afsar291722@gmail.com>
2024-03-31 23:27:15 +0600

    Inheritance code added

---
+++ Semester 3/Course 01 [Object-Oriented Programming With Python]/Week 2 [OOP]/Module 7 [OOP More]/inheritance.py
@@ -0,0 +1,27 @@
+# Inheritance provides you "is a" relation
+
+class Animal:
+    pass
+
+# Dog is a animal
+class Dog(Animal):
+    pass
+
+# Tiger is a animal
+class Tiger(Animal):
+    pass
+
+class Furniture:
+    pass
+
+# chair is a furniture
+class Chair(Furniture):
+    pass
+
+# Table is a furniture
+class Table(Furniture):
+    pass
+
+# Bed is a furniture
+class Bed(Furniture):
+    pass
